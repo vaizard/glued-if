98259a29a36139122495ca41fc898c8773a9c0d6
Pavel Stratil <pavel@vaizard.org>
2024-01-28 13:31:28 +0000

    grr

--- glued/Config/Migrations/20231009002534_actions.sql
+++ glued/Config/Migrations/20231009002534_actions.sql
@@ -1,15 +1,15 @@
 -- migrate:up
 
 CREATE TABLE `t_if__actions` (
-  `c_service_uuid` binary(16) NOT NULL COMMENT 'IF service UUID',
+  `c_deployment_uuid` binary(16) NOT NULL COMMENT 'IF deployment UUID',
   `c_uuid` binary(16) NOT NULL DEFAULT (uuid_to_bin(uuid(),true)) COMMENT 'IF action uuid (v4)',
   `c_data` json NOT NULL COMMENT 'JSON data',
   `c_scheme` varchar(255) GENERATED ALWAYS AS (json_unquote(json_extract(`c_data`,_utf8mb4'$.scheme'))) STORED COMMENT '[STORED] Interface Framework action scheme',
   `c_note` varchar(255) GENERATED ALWAYS AS (json_unquote(json_extract(`c_data`,_utf8mb4'$.note'))) STORED COMMENT '[STORED] Interface Framework action note',
   `c_freq` varchar(255) GENERATED ALWAYS AS (json_unquote(json_extract(`c_data`,_utf8mb4'$.freq'))) STORED COMMENT '[STORED] Interface Framework action frequency',
   PRIMARY KEY (`c_uuid`),
-  UNIQUE KEY `uniq_service_scheme` (`c_service_uuid`,`c_scheme`),
-  KEY `idx_service` (`c_service_uuid`),
+  UNIQUE KEY `uniq_service_scheme` (`c_deployment_uuid`,`c_scheme`),
+  KEY `idx_service` (`c_deployment_uuid`),
   KEY `idx_scheme` (`c_scheme`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='Integration framework service instance actions.';
 
--- glued/Controllers/IfController.php
+++ glued/Controllers/IfController.php
@@ -43,7 +43,7 @@ class IfController extends AbstractController
     public function runs_r1(Request $request, Response $response, array $args = []): Response
     {
         $rp = $this->utils->getQueryParams($request) ?? [];
-        $qs = (new Sql)->q['runs'];
+        $qs = (new \Glued\Lib\IfSql())->q['json:runs:all'];
         $qs = (new \Glued\Lib\QueryBuilder())->select($qs);
         $qs = $this->utils->mysqlQueryFromRequest($qs, $rp, 'c_data');
         $r = $this->mysqli->execute_query($qs,array_values($rp));
@@ -84,32 +84,34 @@ class IfController extends AbstractController
         return $response->withJson($payload);
     }
 
-    public function services_c1(Request $request, Response $response, array $args = []): Response
+    public function deployments_c1(Request $request, Response $response, array $args = []): Response
     {
-        $payload = (array) $request->getBody()->getContents();
-        $payload = json_decode($payload[0], true);
+        $params = $request->getQueryParams();
+        $contentTypeHeader = $request->getHeaderLine('Content-Type') ?? '';
+        if ($contentTypeHeader !== 'application/json') { throw new \Exception('Invalid Content-Type. Please set `Content-Type: application/json', 400); }
+        $payload = $request->getParsedBody();
+        foreach ($payload as $item) {
+            $svc_uuid = \Ramsey\Uuid\Uuid::uuid4()->toString();
+            $svc_data = json_encode($item['deployment']);
+            $res = $this->mysqli->execute_query('SELECT bin_to_uuid(`c_uuid`,true) as svc_uuid FROM `t_if__deployments` WHERE JSON_CONTAINS(c_data, ?, "$")', [$svc_data]);
+            foreach ($res as $r) { $svc_uuid = $r['svc_uuid']; break; }
+            $this->mysqli->execute_query("INSERT INTO `t_if__deployments` ( `c_uuid`, `c_data` ) VALUES ( UUID_TO_BIN(?, true), ? ) ON DUPLICATE KEY UPDATE `c_data` = values(`c_data`)", [$svc_uuid, $svc_data]);
 
-        $svc_uuid = \Ramsey\Uuid\Uuid::uuid4()->toString();
-        $data = $payload['svc'];
-        $data = [ 'c_uuid' => $svc_uuid, 'c_data' => json_encode($data) ];
-        $x = $this->db->rawQuery('INSERT INTO `t_if__services` ( `c_uuid`, `c_data` ) VALUES ( UUID_TO_BIN(?, true), ? )', $data);
-
-        foreach ($payload['act'] as $a) {
-            $act_uuid = \Ramsey\Uuid\Uuid::uuid4()->toString();
-            $data = [ 'c_svc_uuid' => $svc_uuid, 'c_uuid' => $act_uuid, 'c_data' => json_encode($a) ];
-            $this->db->rawQuery('INSERT INTO `t_if__actions` ( `c_svc_uuid`, `c_uuid`, `c_data` ) VALUES ( UUID_TO_BIN(?, true), UUID_TO_BIN(?, true), ? )', $data);
+            foreach ($item['actions'] as $a) {
+                $act_uuid = \Ramsey\Uuid\Uuid::uuid4()->toString();
+                $data = ['c_deployment_uuid' => $svc_uuid, 'c_uuid' => $act_uuid, 'c_data' => json_encode($a)];
+                $this->db->rawQuery('INSERT INTO `t_if__actions` ( `c_deployment_uuid`, `c_uuid`, `c_data` ) VALUES ( UUID_TO_BIN(?, true), UUID_TO_BIN(?, true), ? ) ON DUPLICATE KEY UPDATE `c_data` = VALUES(`c_data`)', $data);
+            }
         }
-
         return $response->withJson($payload);
     }
-
 
 
     public function queue_r1(Request $request, Response $response, array $args = []): Response
     {
         $rp = $this->utils->getQueryParams($request) ?? [];
         $override = [ [ 'next_in', '<', '0' ], [ 'row_num', '=', '1' ] ];
-        $qs = (new Sql)->q['queue'];
+        $qs = (new \Glued\Lib\IfSql())->q['json:runs:latest'];;
         $qs = (new \Glued\Lib\QueryBuilder())->select($qs);
         $qs = $this->utils->mysqlQueryFromRequest($qs, $rp, 'svc_data', override: $override);
         $r = $this->mysqli->execute_query($qs,array_values($rp));
@@ -127,20 +129,16 @@ class IfController extends AbstractController
 
         $data = [];
         $rp = $this->utils->getQueryParams($request) ?? [];
-        $qs = (new Sql)->q['base1'];
-        //if ($args['svc'] ?? false) { $data[] = $args['svc']; $qs.= " and subquery.service = ?"; }
+        $qs = (new \Glued\Lib\IfSql())->q['json:runs:latest'];
+        if ($args['svc'] ?? false) { $data[] = $args['svc']; $qs.= " and subquery.service = ?"; }
         $data = array_values($rp) + $data;
-        $data = [];
-        $qs .= " and subquery.service = 'fio_cz'";
         $res = $this->mysqli->execute_query($qs, $data);
         foreach ($res as $row) {
-           // print_r($row); die();
-            $data = json_decode($row['json_result']); break; }
-        //print_r($data);
+            $data = json_decode($row['json_result'], true); break; }
         $fin['status'] = 'ok';
-        foreach ($data as &$i) {
-            //print_r($i); die();
-            //$i->runs = $this->settings['glued']['protocol'] . $this->settings['glued']['hostname'] . '/api/if/svc/' . $i['svc_type'] . '/act/' . $i['act_uuid'];
+        $base = "{$this->settings['glued']['protocol']}{$this->settings['glued']['hostname']}/api/if";
+        foreach ($data as $k => &$i) {
+            $data[$k]['links']['start'] = "{$base}/svc/{$i['service']}/v1/act/{$i['action']['uuid']}";
             //$i['run'] = $this->settings['glued']['protocol'] . $this->settings['glued']['hostname'] . '/api/if/v1/runs/' . $i['act_uuid'];
         }
         $fin['data'] = $data;
